2e6e9354feff6d5b7b3f1fcde6a89b9eada73c4f
Nahal <nahal.miri.a@gmail.com>
2026-07-12 21:43:56 +0330

    Implement Album entity class

--- Entities/album.cpp
+++ Entities/album.cpp
@@ -1,3 +1,38 @@
 #include "album.h"
 
-Album::Album() {}
+Album::Album(std::string name, int albumId, int artistId)
+    : albumName(name),
+    albumId(albumId),
+    artistId(artistId) {}
+
+std::string Album::getName() const {
+    return this->albumName;
+}
+
+int Album::getAlbumId() const {
+    return this->albumId;
+}
+
+int Album::getArtistId() const {
+    return this->artistId;
+}
+
+const std::vector<std::shared_ptr<Song>>& Album::getSongs() const {
+    return this->songs;
+}
+void Album::setName(const std::string& name) {
+    this->albumName = name;
+}
+
+void Album::addSong(const std::shared_ptr<Song>& newSong) {
+    songs.push_back(newSong);
+}
+
+void Album::removeSong(int songId) {
+    for(int i = 0; i < songs.size(); i++) {
+        if(songs[i]->getSongId() == songId) {
+            songs.erase(songs.begin() + i);
+            return;
+        }
+    }
+}
--- Entities/album.h
+++ Entities/album.h
@@ -1,10 +1,26 @@
 #ifndef ALBUM_H
 #define ALBUM_H
+#include <string>
+#include <vector>
+#include <memory>
+#include "song.h"
 
 class Album
 {
+private:
+    std::string albumName;
+    int albumId;
+    int artistId;
+    std::vector<std::shared_ptr<Song>> songs;
 public:
-    Album();
+    Album(std::string name, int albumId, int artistId);
+    std::string getName() const;
+    int getAlbumId() const;
+    int getArtistId() const;
+    const std::vector<std::shared_ptr<Song>>& getSongs() const;
+    void setName(const std::string& name);
+    void addSong(const std::shared_ptr<Song>& newSong);
+    void removeSong(int songId);
 };
 
 #endif // ALBUM_H
